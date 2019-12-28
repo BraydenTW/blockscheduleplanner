cc7008154c12e019ea3694ed9cd56d8a257bdf11
BraydenTW <brayden.wright90@gmail.com>
2019-12-27 22:14:46 -0600

    Finished basic layout of site

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HS School Schedule Maker</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <button class="save-btn">Save</button>
+    <div class="schedule">
+        <div class="schedule-grid">
+            <div class="term">
+                <div class="year-title">Freshman</div>
+                <div class="term-title">Term 1</div>
+                <div class="term-title">Term 2</div>
+                <div class="term-title">Term 3</div>
+                <div class="term-title">Term 4</div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 1</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 2</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 3</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 4</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+        </div>
+    </div>
+    <div class="schedule">
+        <div class="schedule-grid">
+            <div class="term">
+                <div class="year-title">Sophmore</div>
+                <div class="term-title">Term 1</div>
+                <div class="term-title">Term 2</div>
+                <div class="term-title">Term 3</div>
+                <div class="term-title">Term 4</div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 1</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 2</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 3</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 4</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+        </div>
+    </div>
+    <div class="schedule">
+        <div class="schedule-grid">
+            <div class="term">
+                <div class="year-title">Junior</div>
+                <div class="term-title">Term 1</div>
+                <div class="term-title">Term 2</div>
+                <div class="term-title">Term 3</div>
+                <div class="term-title">Term 4</div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 1</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 2</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 3</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 4</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+        </div>
+    </div>
+    <div class="schedule">
+        <div class="schedule-grid">
+            <div class="term">
+                <div class="year-title">Senior</div>
+                <div class="term-title">Term 1</div>
+                <div class="term-title">Term 2</div>
+                <div class="term-title">Term 3</div>
+                <div class="term-title">Term 4</div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 1</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 2</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 3</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+            <div class="block">
+                <div class="block-title">Block 4</div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+                <div class="block-item" contenteditable="true"></div>
+            </div>
+        </div>
+    </div>
+    <?php
+        $jsonFile = file_get_contents("./js/data.json");
+        $jsonData = json_decode($jsonFile, true);
+        echo '<script>';
+        echo 'var jsonData = ' . json_encode($jsonData) . ';';
+        echo '</script>';
+    ?>
+    <script src="./js/scripts.js"></script>
+    <script>
+        document.querySelector(".save-btn").addEventListener("click", () => {
+            var stringedObj = JSON.stringify(jsonData);
+            window.location.href = "http://localhost:5500/save.php?data=" + stringedObj;
+        });
+        document.querySelectorAll(".block-item").forEach((item, index) => {
+            item.textContent = jsonData[index].class;
+        });
+    </script>
+</body>
+</html>
