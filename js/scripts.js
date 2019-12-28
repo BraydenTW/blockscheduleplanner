fe3429151d48cee6389963427a64373dca7dbf71
BraydenTW <brayden.wright90@gmail.com>
2019-12-28 10:50:49 -0600

    Changed all .textContent to .value

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 let secondItem;
 
 document.querySelectorAll(".block-item").forEach((item, index) => {
-    item.addEventListener("input", () => {
-        jsonData[index].class = item.textContent;
+    item.addEventListener("input", ev => {
+        if (item.value.length < 30) {
+            jsonData[index].class = item.value;
+        } else {
+            ev.preventDefault();
+            item.substr(1, item.length);
+        }
     });
 });
 
@@ -10,17 +15,17 @@ document.querySelectorAll(".block-item").forEach(item => {
     item.setAttribute("draggable", "true");
     item.addEventListener("drag", ev => {
         ev.preventDefault();
-        localStorage.setItem("block1_txt", item.textContent);
+        localStorage.setItem("block1_txt", item.value);
         secondItem = item;
     });
     item.addEventListener("drop", ev => {
         ev.preventDefault();
-        localStorage.setItem("block2_txt", item.textContent);
+        localStorage.setItem("block2_txt", item.value);
         console.log(secondItem);
-        item.textContent = localStorage.getItem("block1_txt");
-        secondItem.textContent = localStorage.getItem("block2_txt");
+        item.value = localStorage.getItem("block1_txt");
+        secondItem.value = localStorage.getItem("block2_txt");
         document.querySelectorAll(".block-item").forEach((item, index) => {
-            jsonData[index].class = item.textContent;
+            jsonData[index].class = item.value;
         });
     });
     item.addEventListener("dragover", ev => {
